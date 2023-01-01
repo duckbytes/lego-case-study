6caa25e30db5a6d711c42f141ac64623a2a63c5c
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-01 23:28:14 +0000

    more imports to __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+ma = Marshmallow(app)
 
 
 from ariadne import (
     load_schema_from_path,
     make_executable_schema,
-    graphql_sync,
     snake_case_fallback_resolvers,
     ObjectType,
 )
@@ -25,4 +26,4 @@ query.set_field("preferredItem", queries.resolve_preferred_item)
 type_defs = load_schema_from_path("schema.graphql")
 schema = make_executable_schema(type_defs, query, snake_case_fallback_resolvers)
 
-from app import models, routes
+from app import models, routes, determine_preferred_item, schemas
