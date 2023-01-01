f30535aaf18c4bb4e6be59f93be0d8541002a244
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-01 23:29:08 +0000

    query for finding preferred item

--- app/queries.py
+++ app/queries.py
@@ -1,9 +1,18 @@
-from app import models
+from app.determine_preferred_item import determine_preferred_item
+from app import models, schemas
 from flask import jsonify
 
 
-def resolve_preferred_item(_, info):
+item_schema = schemas.ItemSchema()
+
+
+def resolve_preferred_item(_, info, brickIds):
     try:
-        return {"item": {"id": "test"}}
+        bricks = models.Brick.query.filter(models.Brick.id.in_(brickIds)).all()
+        preferred_item = determine_preferred_item(bricks)
+        if preferred_item:
+            return {"item": item_schema.dump(preferred_item)}
+        else:
+            return {"item": None}
     except Exception as e:
-        return {"errors": [str(e)]}
+        return {"item": None, "errors": [str(e)]}
