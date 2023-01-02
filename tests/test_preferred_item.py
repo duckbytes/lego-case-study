30a85dce573d5c6a9a5563a40d0f50fba95eea16
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-02 03:34:38 +0000

    placeholder test for fail case

--- tests/test_preferred_item.py
+++ tests/test_preferred_item.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 from app import models
 
@@ -36,3 +38,43 @@ def test_preferred_item(client, preferred_item):
     assert response.status_code == 200
     data = response.get_json()["data"]
     assert data["preferredItem"]["item"]["id"] == str(expected_item.id)
+
+
+@pytest.mark.parametrize(
+    "status",
+    [
+        models.ItemStatus.Normal,
+    ],
+)
+@pytest.mark.usefixtures("mocker")
+def test_preferred_item_failure(mocker, client, preferred_item):
+    # the mock doesn't work here
+    # left as a placeholder for now
+    return
+    mock_determine_preferred_item = MagicMock(side_effect=Exception("test exception"))
+    mocker.patch(
+        "app.determine_preferred_item",
+        mock_determine_preferred_item,
+    )
+
+    bricks = preferred_item["bricks"]
+
+    brick_ids = ['"{}"'.format(brick.id) for brick in bricks]
+    brick_id_strings = ", ".join(brick_ids)
+    query = """
+    query {
+        preferredItem(brickIds: [%s]) {
+            errors
+            item {
+                id
+            }
+        }
+    }
+    """ % (
+        brick_id_strings
+    )
+    response = client.post("/graphql", json={"query": query})
+
+    assert response.status_code == 200
+    data = response.get_json()["data"]
+    assert data["preferredItem"]["errors"][0] == ["test exception"]
