7a77e7bde99692f64856b32d2de41429c8cf87b6
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-02 00:01:46 +0000

    fix marshmallow status field failing

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,5 @@
-from marshmallow import (
-    fields,
-)
+from marshmallow import fields
+from marshmallow_sqlalchemy import field_for
 from app import models, ma
 
 
@@ -10,7 +9,7 @@ class BrickSchema(ma.SQLAlchemySchema):
 
         fields = ("id", "design_id", "color_ids")
 
-    id = fields.Integer(dump_only=True)
+    id = field_for(models.Item, "id", dump_only=True)
     design_id = fields.Integer(required=True)
     color_ids = fields.List(fields.Integer())
 
@@ -21,9 +20,9 @@ class MasterDataSchema(ma.SQLAlchemySchema):
 
         fields = ("id", "price", "status")
 
-    id = fields.Integer(dump_only=True)
-    price = fields.Integer(required=True)
-    status = fields.String(required=True)
+    id = field_for(models.MasterData, "id", dump_only=True)
+    price = fields.Integer()
+    status = fields.Enum(models.ItemStatus)
 
 
 class ItemSchema(ma.SQLAlchemySchema):
@@ -32,6 +31,6 @@ class ItemSchema(ma.SQLAlchemySchema):
 
         fields = ("id", "bricks", "master_data")
 
-    id = fields.Integer(dump_only=True)
+    id = field_for(models.Item, "id", dump_only=True)
     bricks = fields.Nested(BrickSchema, many=True)
     master_data = fields.Nested(MasterDataSchema)
