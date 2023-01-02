6c56b96a3d8813377ffc078c0ee02bf08c4c459a
Theo Cranmore <theo.cranmore@gmail.com>
2023-01-02 02:59:17 +0000

    files for running with docker

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") or "postgresql://localhost/lego"
+        os.environ.get("SQLALCHEMY_DATABASE_URI") or "postgresql://localhost/lego"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
